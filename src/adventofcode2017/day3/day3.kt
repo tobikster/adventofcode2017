0e53867b7a538c06652b55229ad914c991ed16b2
Paweł Tobiszewski <pawel.tobiszewski@gmail.com>
2018-01-05 17:13:32 +0100

    Refactor day 3

--- src/adventofcode2017/day3/day3.kt
+++ src/adventofcode2017/day3/day3.kt
@@ -2,17 +2,23 @@ package adventofcode2017.day3
 
 const val INPUT = 265149
 
-fun main(args: Array<String>) {
-    val size = 1000
-    val memory = Memory(size)
+fun part1(input: Int): Int {
+    return Memory(1).distanceToCenter(input)
+}
 
+fun part2(input: Int): Int {
+    val memory = Memory(13)
     var position = 0
     var value = 1
-    while (value < INPUT) {
+    while (value < input) {
         memory[position] = value
         position += 1
         value = memory.getNeighboursSum(position)
     }
-    println("Part 1 answer: ${memory.distanceToCenter(INPUT)}")
-    println("Part 2 answer: $value")
+    return value
+}
+
+fun main(args: Array<String>) {
+    println("Part 2 answer: ${part1(INPUT)}")
+    println("Part 2 answer: ${part2(INPUT)}")
 }
